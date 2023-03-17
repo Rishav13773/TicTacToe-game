7a4f794c8f5bc8cc244d2f339bf2bf6b6f78112d
Rishav <ahoul72@gmail.com>
2023-03-17 13:49:04 +0530

    added player 2

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -3,18 +3,24 @@ import Square from './Square'
 
 const Board = () => {
     const [squares, setSquares] = useState(Array(9).fill(null)); //initializing an array using array constructor
+    const [isXNext, setIsXNext] = useState(true);
     console.log(squares)
 
     const handleSquareClick = clickedPosition => {
+        if (squares[clickedPosition]) { //this condition checking if we already have 'X' or 'O' in our square then will exit the function
+            return;
+        }
+
         setSquares((currentSquare) => {
             return currentSquare.map((squareValue, position) => {
                 if (clickedPosition === position) {
-                    return 'X'
+                    return isXNext ? 'X' : 'O'
                 }
 
                 return squareValue
             })
         })
+        setIsXNext(currentIsXNext => !currentIsXNext);
     }
 
     const renderSquare = position => { //created this function for to make code more readable
