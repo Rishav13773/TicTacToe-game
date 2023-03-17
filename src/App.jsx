e98a4fd4c286253c119283f8187abdd1b32c521d
Rishav <ahoul72@gmail.com>
2023-03-17 11:50:25 +0530

    game board created

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Board from './components/Board'
+import Square from './components/Square'
+import './style.scss'
+
+const App = () => {
+  return (
+    <div className='app'>
+
+      <Board />
+    </div>
+  )
+}
+
+export default App
